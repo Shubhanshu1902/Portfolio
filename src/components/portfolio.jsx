1873e24bdb620f75784d413e9450b6288f05ff8c
shubhanshu1902 <shubhanshu1902@gmail.com>
2022-09-06 12:47:26 +0530

    Added Project

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -37,7 +37,7 @@ class Portfolio extends React.Component {
                         <h2 className="w-title">Lorem Ipsum</h2>
                         <div className="w-more">
                           <span className="w-ctegory">
-                            HTML5 CSS3 Bootstrap ReactJS
+                            Attack On Ghost
                           </span>
                         </div>
                       </div>
